da62165bd8994a17ef345a72227a76388759f340
hsrahman <rahmanh4@aston.ac.uk>
2017-06-12 19:47:36 +0100

    Creating network utility class

--- app/src/main/java/com/example/hamidur/mynews/NewsActivity.java
+++ app/src/main/java/com/example/hamidur/mynews/NewsActivity.java
@@ -2,12 +2,34 @@ package com.example.hamidur.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private NewsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        ArrayList<NewsArticle> articles = new ArrayList<NewsArticle>();
+        articles.add(new NewsArticle("https://image.flaticon.com/teams/new/1-freepik.jpg", "test description", "Test title", "Mr Author", "08/07/1996"));
+
+
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new NewsAdapter(this, articles);
+
+
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(mAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/hamidur/mynews/QueryUtils.java
@@ -0,0 +1,19 @@
+package com.example.hamidur.mynews;
+
+/**
+ * Created by pro-developer Hamidur on 12/06/2017.
+ */
+public class QueryUtils {
+    private static String LOG_TAG = "EXCEPTION";
+
+
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
+
+
+}
