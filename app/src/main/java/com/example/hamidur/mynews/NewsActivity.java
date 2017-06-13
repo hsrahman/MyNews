63e4af8223fd4b837c01602ca58a980ff8e529ec
rahmann5 <rahmann5@aston.ac.uk>
2017-06-13 15:57:44 +0100

    Preparation for loader p2

--- app/src/main/java/com/example/hamidur/mynews/NewsActivity.java
+++ app/src/main/java/com/example/hamidur/mynews/NewsActivity.java
@@ -1,14 +1,29 @@
 package com.example.hamidur.mynews;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsArticle>> {
 
     private NewsAdapter mAdapter;
+
+    private static final int ARTICLE_LOADER_ID = 1;
+
+    private static final String NEWSAPI_REQUEST_URL = " https://newsapi.org/v1/articles";
+
+    private TextView emptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +40,45 @@ public class NewsActivity extends AppCompatActivity {
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new NewsAdapter(this, articles);
 
-
+        emptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(emptyStateTextView);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(mAdapter);
 
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()){
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
     }
+
+    @Override
+    public Loader<List<NewsArticle>> onCreateLoader(int i, Bundle bundle){
+        Uri baseUri = Uri.parse(NEWSAPI_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("source", "the-next-web");
+        uriBuilder.appendQueryParameter("sortBy", "latest");
+        uriBuilder.appendQueryParameter("apiKey", API_KEY);
+
+        return new NewsLoader(this, uriBuilder.toString());
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<NewsArticle>> loader){
+        mAdapter.clear();
+    }
+
 }
