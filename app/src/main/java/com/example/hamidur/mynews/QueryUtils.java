746aad26f39a6012367bf30b24dc7e1b2a25da31
hsrahman <rahmanh4@aston.ac.uk>
2017-06-13 14:34:41 +0100

    Network Utility Class http request and read input done

--- app/src/main/java/com/example/hamidur/mynews/QueryUtils.java
+++ app/src/main/java/com/example/hamidur/mynews/QueryUtils.java
@@ -1,19 +1,76 @@
 package com.example.hamidur.mynews;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 /**
  * Created by pro-developer Hamidur on 12/06/2017.
  */
 public class QueryUtils {
     private static String LOG_TAG = "EXCEPTION";
 
-
-    /**
-     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-     * This class is only meant to hold static variables and methods, which can be accessed
-     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-     */
     private QueryUtils() {
     }
 
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
 
+    private static String createHttpRequest (URL url) throws IOException{
+        String jsonResponse = "";
+        if (url != null) {
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.connect();
+                if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                } else{
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+            } finally {
+                if (urlConnection != null) urlConnection.disconnect();
+                if (inputStream != null) inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream (InputStream inputStream) throws IOException{
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line;
+            do {
+                line = reader.readLine();
+                output.append(line);
+            } while (line != null);
+        }
+        return output.toString();
+    }
 }
