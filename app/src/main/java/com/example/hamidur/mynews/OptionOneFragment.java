8607f046573e98e74e39d16dd56a89e9cc065bfd
hsrahman <rahmanh4@aston.ac.uk>
2017-06-19 13:03:44 +0100

    Option one fragment done (i think)

--- app/src/main/java/com/example/hamidur/mynews/OptionOneFragment.java
+++ app/src/main/java/com/example/hamidur/mynews/OptionOneFragment.java
@@ -3,7 +3,10 @@ package com.example.hamidur.mynews;
 
 import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +14,8 @@ import android.support.v4.util.ArraySet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,7 +25,7 @@ import java.util.Set;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class OptionOneFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<List<NewsArticle>>> {
+public class OptionOneFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<NewsArticle>> {
 
     private NewsAdapter mAdapter;
     private static final String API_KEY = "b5b4806ba6834681baecc6492d59d788";
@@ -41,13 +46,12 @@ public class OptionOneFragment extends Fragment implements LoaderManager.LoaderC
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.news_list, container, false);
 
-        /* News Activity OnCreate
 
-             ListView newsListView = (ListView) findViewById(R.id.list);
+        ListView newsListView = (ListView) rootView.findViewById(R.id.list);
 
-        mAdapter = new NewsAdapter(this, new ArrayList<NewsArticle>());
+        mAdapter = new NewsAdapter(getActivity(), new ArrayList<NewsArticle>());
 
-        emptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        emptyStateTextView = (TextView) rootView.findViewById(R.id.empty_view);
         newsListView.setEmptyView(emptyStateTextView);
 
         newsListView.setAdapter(mAdapter);
@@ -63,27 +67,25 @@ public class OptionOneFragment extends Fragment implements LoaderManager.LoaderC
         });
 
         ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        if(networkInfo != null && networkInfo.isConnected()){
-            LoaderManager loaderManager = getLoaderManager();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getActivity().getLoaderManager();
             loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
         } else {
-            View loadingIndicator = findViewById(R.id.loading_indicator);
+            View loadingIndicator = rootView.findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
             emptyStateTextView.setText(R.string.no_internet_connection);
         }
-
-         */
 
 
         return rootView;
     }
 
     @Override
-    public void onLoadFinished(Loader<List<List<NewsArticle>>> loader, List<List<NewsArticle>> newsArticles) {
+    public void onLoadFinished(Loader<List<NewsArticle>> loader, List<NewsArticle> newsArticles) {
         View loadingIndicator = getActivity().findViewById(R.id.loading_indicator);
         loadingIndicator.setVisibility(View.GONE);
 
@@ -92,35 +94,31 @@ public class OptionOneFragment extends Fragment implements LoaderManager.LoaderC
         mAdapter.clear();
 
         if (newsArticles != null && !newsArticles.isEmpty()) {
-            List<NewsArticle> allArticles = new ArrayList<>();
-            for (List<NewsArticle> articles : newsArticles) {
-                if (articles != null) allArticles.addAll(articles);
-            }
-            // randomis the news articles
-
-            mAdapter.addAll(allArticles);
+            mAdapter.addAll(newsArticles);
         }
 
         getLoaderManager().destroyLoader(ARTICLE_LOADER_ID);
     }
 
     @Override
-    public Loader<List<List<NewsArticle>>> onCreateLoader(int i, Bundle bundle){
-        List<String> allUrls = new ArrayList<>();
+    public Loader<List<NewsArticle>> onCreateLoader(int i, Bundle bundle) {
         Set<String> ids = getActivity().getSharedPreferences("my_sources", Context.MODE_PRIVATE).getStringSet("source", new ArraySet<String>());
+        Uri baseUri = Uri.parse(NEWSAPI_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        int counter = 0;
         for (String id : ids) {
-            Uri baseUri = Uri.parse(NEWSAPI_REQUEST_URL);
-            Uri.Builder uriBuilder = baseUri.buildUpon();
-            uriBuilder.appendQueryParameter("source", id);
-            //uriBuilder.appendQueryParameter("sortBy", "latest");
-            uriBuilder.appendQueryParameter("apiKey", API_KEY);
-            allUrls.add(uriBuilder.toString());
+            counter++;
+            if (counter == 1) {
+                uriBuilder.appendQueryParameter("source", id);
+                //uriBuilder.appendQueryParameter("sortBy", "latest");
+                uriBuilder.appendQueryParameter("apiKey", API_KEY);
+            }
         }
-        return new NewsLoader(getActivity(), allUrls);
+        return new NewsLoader(getActivity(), uriBuilder.toString());
     }
 
     @Override
-    public void onLoaderReset(Loader<List<List<NewsArticle>>> loader){
+    public void onLoaderReset(Loader<List<NewsArticle>> loader) {
         mAdapter.clear();
     }
 
