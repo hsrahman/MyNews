faf4a529868da3066c35916b1f2e0baf45190327
hsrahman <rahmanh4@aston.ac.uk>
2017-06-21 12:23:42 +0100

    order by page setup p1

--- app/src/main/java/com/example/hamidur/mynews/QueryUtils.java
+++ app/src/main/java/com/example/hamidur/mynews/QueryUtils.java
@@ -99,7 +99,12 @@ public class QueryUtils {
                 String id = currentSource.getString("id");
                 String name = currentSource.getString("name");
                 String category = currentSource.getString("category");
-                sourcesList.add(new Source(id, name, category));
+                JSONArray orderByJson = currentSource.getJSONArray("sortBysAvailable");
+                String [] orderBy = new String[orderByJson.length()];
+                for (int j = 0; j < orderByJson.length(); j++) {
+                    orderBy[i] = orderByJson.getString(j);
+                }
+                sourcesList.add(new Source(id, name, category, orderBy));
             }
         } catch (JSONException e) {
             Log.e("QueryUtils", "Problem parsing the source JSON results", e);
