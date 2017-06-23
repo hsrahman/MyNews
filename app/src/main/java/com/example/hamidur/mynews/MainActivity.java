a2c4f4d1c8e2b608aa390cb4cd3772f0d0d69f91
rahmann5 <rahmann5@aston.ac.uk>
2017-06-23 16:29:13 +0100

    minor change

--- app/src/main/java/com/example/hamidur/mynews/MainActivity.java
+++ app/src/main/java/com/example/hamidur/mynews/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.hamidur.mynews;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +8,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
