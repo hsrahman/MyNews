8789b62eb02a581f9c71bb2dd47da6172b9c6958
hsrahman <rahmanh4@aston.ac.uk>
2017-06-24 19:30:25 +0100

    remove sources from my source list problem

--- app/src/main/java/com/example/hamidur/mynews/SpinnerAdapter.java
+++ app/src/main/java/com/example/hamidur/mynews/SpinnerAdapter.java
@@ -5,9 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckedTextView;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
