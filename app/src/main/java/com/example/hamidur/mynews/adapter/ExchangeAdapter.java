9123caf88730967d90c9da3d909a7e55659a01c5
hsrahman <rahmanh4@aston.ac.uk>
2018-08-18 11:51:36 +0100

    done exchange adapter (not tested)

--- app/src/main/java/com/example/hamidur/mynews/adapter/ExchangeAdapter.java
+++ app/src/main/java/com/example/hamidur/mynews/adapter/ExchangeAdapter.java
@@ -5,16 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.hamidur.mynews.R;
 import com.example.hamidur.mynews.model.ExchangeRate;
 import com.example.hamidur.mynews.utility.QueryUtils;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -23,6 +25,9 @@ import java.util.List;
  */
 
 public class ExchangeAdapter extends ArrayAdapter<ExchangeRate> {
+
+    private List<String> allCurrencyCodes;
+    private List<String> countryCurrency;
 
     public ExchangeAdapter(Context context, List<ExchangeRate> exchangeRates) {
         super(context, 0, exchangeRates);
@@ -35,28 +40,39 @@ public class ExchangeAdapter extends ArrayAdapter<ExchangeRate> {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.exchange_rate_item, parent, false);
         }
-
+        readJsonCurrencies ();
         ExchangeRate rate = getItem(position);
+        TextView countryExrName = (TextView) listItemView.findViewById(R.id.ex_country_name);
+        TextView countryExrValue = (TextView) listItemView.findViewById(R.id.ex_rate);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item,
+                allCurrencyCodes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner s = (Spinner) listItemView.findViewById(R.id.country_ex_spinner);
+        s.setAdapter(adapter);
 
-
+        s.setSelection(adapter.getPosition(rate.getTo()));
+        countryExrName.setText(countryCurrency.get(adapter.getPosition(rate.getTo())));
+        countryExrValue.setText(rate.getVal());
 
         return listItemView;
     }
 
 
     private void readJsonCurrencies () {
+        allCurrencyCodes = new ArrayList<>();
+        countryCurrency = new ArrayList<>();
         try {
             JSONObject currencyObj = new JSONObject(QueryUtils.readFromRawJsonFile(getContext().getResources(),R.raw.currencies));
             JSONObject result = currencyObj.getJSONObject("results");
-            Iterator<?> allCurrencyCodes = result.keys();
-            while( allCurrencyCodes.hasNext() ) {
-                String key = (String)allCurrencyCodes.next();
+            Iterator<?> currencyCodes = result.keys();
+            while( currencyCodes.hasNext() ) {
+                String key = (String)currencyCodes.next();
                 if ( result.get(key) instanceof JSONObject ) {
-
+                    allCurrencyCodes.add(key);
+                    countryCurrency.add(result.getJSONObject(key).getString("currencyName"));
                 }
             }
-
-
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (IOException e) {
